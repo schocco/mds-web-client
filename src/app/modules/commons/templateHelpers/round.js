78f8d35313ccb9bb94b6cc4edbd3c19a733bf9cd
Rocco Schulz <schocco.rulz@gmail.com>
2015-07-04 13:16:29 +0200

    Change trail item style and make whole div clickable. Alter round templatehelper.

--- src/app/modules/commons/templateHelpers/round.js
+++ src/app/modules/commons/templateHelpers/round.js
@@ -1,8 +1,15 @@
 /**
  * Rounds to the nearest integer.
  * @param value must be a number
+ * @param precision digits behind the floating point
  * @returns {number}
  */
-module.exports = function(value) {
-    return Math.round(value);
+module.exports = function(value, precision, data) {
+    if(data === undefined || precision < 0) {
+        // handlebars passes a context object into each funtion as the last argument,
+        // when the data argument is undefined, then no precision was specified in the template
+        precision = 0;
+    }
+    var div = Math.pow(10, precision);
+    return Math.round(value * div) / div;
 };
