fab9feec6986ac6ee56fd4814b9b188897f5e0f6
Rocco Schulz <schocco.rulz@gmail.com>
2015-07-04 18:27:50 +0200

    Show example calculation in calculator when button clicked.

--- src/app/modules/scales/views/Udh.js
+++ src/app/modules/scales/views/Udh.js
@@ -11,10 +11,31 @@ module.exports = Marionette.LayoutView.extend({
 	regions: {
 		calculator: '[data-region=calculator]'
 	},
+
+    triggers: {
+        "click #showInCalculator": "show:example"
+    },
+
 	template: tpl,
 
     onShow: function() {
-        this.calculator.show(new ScoreView({type: "udh", editable: true}));
+        this.scoreView = new ScoreView({type: "udh", editable: true});
+        this.calculator.show(this.scoreView);
+    },
+
+    /**
+     * Show the example calculation in the calculation view.
+     */
+    onShowExample: function() {
+        //get form fields by name and set values
+        $("#avg_slope>input").val(16);
+        $("#avg_difficulty>select").val(1.5);
+        $("#max_difficulty>select").val(3.5);
+        $("#total_length>input").val(6000);
+        // simulate a click on the update button
+        this.scoreView.onScoreUpdateClicked();
+        $('html,body').animate({scrollTop: $("#calculateScoreForm").offset().top},'slow');
     }
+
 
 });
--- src/app/modules/scales/views/Uxc.js
+++ src/app/modules/scales/views/Uxc.js
@@ -11,8 +11,29 @@ module.exports = Marionette.LayoutView.extend({
 		calculator: '[data-region=calculator]'
 	},
 	template: tpl,
+
+    triggers: {
+        "click #showInCalculator": "show:example"
+    },
+
 	onShow: function() {
-		this.calculator.show(new ScoreView({type: "uxc", editable: true}));
-	}
+        this.scoreView = new ScoreView({type: "uxc", editable: true});
+		this.calculator.show(this.scoreView);
+	},
+
+    /**
+     * Sets the values of the calculation example in the calculator table.
+     */
+    onShowExample: function(){
+        //get form fields by name and set values
+        $("#max_slope_uh>input").val(8);
+        $("#total_ascent>input").val(590);
+        $("#avg_difficulty>:input").val(0.5);
+        $("#max_difficulty>:input").val(1.5);
+        $("#total_length>input").val(42000);
+        // simulate a click on the update button
+        this.scoreView.onScoreUpdateClicked();
+        $('html,body').animate({scrollTop: $("#calculateScoreForm").offset().top},'slow');
+    }
 
 });
